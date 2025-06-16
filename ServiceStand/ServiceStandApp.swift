2258c4270b38a869b11c6f000e2188ad0a740e02
Justin Cotton <jcotton7@gmail.com>
2025-06-15 23:11:36 -0600

    Initial Commit

---
+++ ServiceStand/ServiceStandApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceStandApp.swift
+//  ServiceStand
+//
+//  Created by Justin on 2025-06-15.
+//
+
+import SwiftUI
+
+@main
+struct ServiceStandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
